bf615fc90fe5f99f85de38b9379f7a52ae9497d0
CallumF15 <Callum_flannagan@msn.com>
2014-12-13 00:18:31 +0000

    V1.3
    camera almost there again... ( I hope)

--- GP2BaseCode-Ex8-Starter/headers/Camera.h
+++ GP2BaseCode-Ex8-Starter/headers/Camera.h
@@ -13,7 +13,7 @@
 #include <gl\glew.h>
 
 #include <gl\glew.h>
-#include <glm\gtc\quaternion.hpp>
+//#include <glm\gtc\quaternion.hpp>
 #include <glm/glm.hpp>
 using glm::mat4;
 using glm::vec3;
@@ -23,88 +23,45 @@ using glm::vec3;
 
 #include "Component.h"
 
-enum MovementType { FORWARD, BACKWARD, STRAFE_LEFT, STRAFE_RIGHT, RESET };
+enum MovementType { FORWARD, BACKWARD, STRAFE_LEFT, STRAFE_RIGHT, RESET, UP, DOWN, STEADY };
 
 class Camera:public Component
 {
+	const float movementSpeed = 0.1f;
+
 public:
-    Camera();
+	Camera();
     ~Camera();
     
 	void update();
     
-	void setLook(float x, float y, float z);
-    
-	void setUp(float x, float y, float z);
-    
+	void setLook(float x, float y, float z);  
+	void setUp(float x, float y, float z);   
     void setNearClip(float near);
     void setFarClip(float far);
 	void setFOV(float FOV);
     
 	void setAspectRatio(float ratio);
-    
+  
+
 	mat4& getView();
 	mat4& getProjection();
 
-	void translate(glm::vec3& direction);
-	void applyMovement(MovementType movement);
-	void setMousePosition(int X, int Y);
-
-	void calculateMovement();
-	void setTime(float deltatime);
-
-	void lockCamera();
-	void moveCamera(float distance, float direction);
-	void moveCameraUp(float distance, float direction);
-	void control(SDL_Window* window, float moveVelocity, float mouseVelocity, bool mi);
-	void movement(MovementType movementType, float mouseVelocity, float moveVelocity);
-	void updateCamera();
-
 	MovementType setMovementType(MovementType type);
+	void setMousePosition(int X, int Y, int xVel, int yVel);
+	void movement(MovementType movementType);
+	void mouseUpdate(const glm::vec2& newMousePos);
+	void strafeLeft();
+	void strafeRight();
+
 
 protected:
 private:
-	// Creates an identity quaternion (no rotation)
-	glm::quat MyQuaternion;
+	vec3 strafedirection;
+	glm::vec2 oldMousePos;
+	MovementType m_movementType;
 
-	// Conversion from Euler angles (in radians) to Quaternion
-	vec3 EulerAngles;
-
-
-
-	//my variables
-	MovementType  m_movementType;
-	int m_MouseX, m_MouseY;
-	float mouseSpeed = 0.005f; 
-	float speed = 3.0f;
-
-	float m_deltatime;
-
-	vec3 direction;
-	vec3 right;
-
-	// position
-	//vec3 position = vec3(0, 0, 5);
-
-	// horizontal angle : toward -Z
-	float horizontalAngle = 3.14f;
-	// vertical angle : 0, look at the horizon
-	float verticalAngle = 0.0f;
-
-
-	//yaw/pitch...
-	
-	vec3 lookvec, camPos;
-	vec3 m_Rotation;
-	float camPitch = 0.0f;
-	float camYaw = 0.0f;
-	float camX = 0.0f, camY = 0.0f, camZ = 0.0f;
-	bool mouseIn = false;
-
-	//end myVariables
-
-
-
+	vec3 m_forward;
 	glm::vec3 m_position;
 	glm::vec3 m_direction;
 
--- GP2BaseCode-Ex8-Starter/src/Camera.cpp
+++ GP2BaseCode-Ex8-Starter/src/Camera.cpp
@@ -16,14 +16,12 @@
 #define M_PI 3.1415926525
 #endif
 
+
 Camera::Camera()
 {
 	m_Type = "Camera";
 
-
-	m_Rotation = vec3(0.0f, 0.0f, 0.0f);
-	m_position = vec3(0.0f, 0.0f, 0.0f);
-
+	m_direction = vec3(0.0, 0.0, -1.0f);
 	m_LookAt = vec3(0.0f, 0.0f, 0.0f);
 	m_Up = vec3(0.0f, 1.0f, 0.0f);
 
@@ -33,9 +31,6 @@ Camera::Camera()
 	m_AspectRatio = 16.0f / 9.0f;
 	m_View = mat4();
 	m_Projection = mat4();
-
-
-	m_direction = vec3(1.0, 0.0, 1.0f);
 }
 
 Camera::~Camera()
@@ -46,136 +41,74 @@ Camera::~Camera()
 
 void Camera::update()
 {
-	//get the position from the transform
-	//vec3 position = m_Parent->getTransform()->getPosition();
 	m_position = m_Parent->getTransform()->getPosition();
 	m_Projection = glm::perspective(m_FOV, m_AspectRatio, m_NearClip, m_FarClip);
-	m_View = glm::lookAt(m_position, m_position, m_Up);
-
+	m_View = glm::lookAt(m_position, m_position + m_direction, m_Up);
 }
 
-void Camera::lockCamera()
+void Camera::mouseUpdate(const glm::vec2& newMousePos)
 {
-	if (camPitch > 90)
-		camPitch = 90;
-	if (camPitch < -90)
-		camPitch = -90;
-	if (camYaw < 0.0)
-		camYaw += 360;
-	if (camYaw > 360)
-		camYaw -= 360;
-}
+	glm::vec2 mouseDelta = newMousePos - oldMousePos;
 
-void Camera::moveCamera(float distance, float direction){
-
-	float radian = (camYaw + direction)* M_PI / 180.0;
-	m_position.x -= glm::sin(radian)*distance;
-	m_position.z -= glm::cos(radian)*distance;
-}
-
-void Camera::moveCameraUp(float distance, float direction){
-
-	float radian = (camPitch + direction)* M_PI / 180.0;
-	m_position.y += glm::sin(radian)*distance;
-}
-
-void Camera::control(SDL_Window* window, float moveVelocity, float mouseVelocity, bool mi){
-	if (mi){
-		int midX = 320;
-		int midY = 240;
-
-
-		int tmpx, tmpy;
-		tmpx = m_MouseX;
-		tmpy = m_MouseY;
-
-		camYaw += mouseVelocity * (midX - tmpx);
-		camPitch += mouseVelocity * (midY - tmpy);
-		lockCamera();
-
-		SDL_WarpMouseInWindow(window, midX, midY);
-		movement(m_movementType, mouseVelocity, moveVelocity);
+	if (glm::length(mouseDelta) > 50.0f){
+		oldMousePos = newMousePos;
+		return;
 	}
+	
+	const float ROTATIONAL_SPEED = 0.5f;
+
+	    strafedirection = glm::cross(m_direction, m_Up);
+	    glm::mat4 rotator = glm::rotate(mat4(1.0f), -mouseDelta.x * ROTATIONAL_SPEED, m_Up) *
+		glm::rotate(mat4(1.0f), -mouseDelta.y * ROTATIONAL_SPEED, strafedirection);
+
+	m_direction = glm::mat3(rotator) * m_direction;
+
+	oldMousePos = newMousePos; 
 }
 
-void Camera::movement(MovementType movementType, float mouseVelocity, float moveVelocity)
+
+
+
+void Camera::movement(MovementType movementType)
 {
 	switch (movementType){
 
 	case  FORWARD:
-		if (camPitch != 90 && camPitch != -90)
-			moveCamera(moveVelocity, 0.0);
-		moveCameraUp(moveVelocity, 0.0);
+		m_position += movementSpeed * m_direction;
 		break;
 	case BACKWARD:
-		if (camPitch != 90 && camPitch != -90)
-			moveCamera(moveVelocity, 180.0);
-		moveCameraUp(moveVelocity, 180.0);
+		m_position += -movementSpeed * m_direction;
 		break;
 	case STRAFE_LEFT:
-		moveCamera(moveVelocity, 90.0);
+		strafeLeft();
 		break;
 	case STRAFE_RIGHT:
-		moveCamera(moveVelocity, 270.0);
+		strafeRight();
+		break;
+	case UP:
+		m_position += movementSpeed * m_Up;
+		break;
+	case DOWN:
+		m_position -= movementSpeed * m_Up;
 		break;
 	case RESET:
-		m_position = vec3(0.0, 0.0, -10.0f);
+
 		break;
 	}
-
-	//int lookz = m_position.x + (int)(30 * glm::cos(camYaw));
-	//int lookx = m_position.x + (int)(30 * glm::sin(camYaw));
-	//int looky = m_position.y + (int)(30 * glm::tan(camPitch));
-	//lookvec = vec3(lookx, looky, lookz);
-
-	//direction.x = -sin(m_Rotation.y);
-	//direction.y = sin(m_Rotation.x);
-	//direction.z = cos(m_Rotation.y);
-
-
-	//glm::normalize(direction);
-
-	//m_position.y += sin(m_Rotation.x) ;
-	//m_position += direction;
-	
-
-
-	//m_Rotation.x = (-camPitch, 1.0, 0.0, 0.0);
-	//m_Rotation.y = (-camYaw, 0.0, 1.0, 0.0);
-
-	glLoadIdentity();
-	glRotatef(-camPitch, 1.0, 0.0, 0.0);
-	glRotatef(-camYaw, 0.0, 1.0, 0.0);
 }
 
-void Camera::updateCamera(){
-	glTranslatef(-m_position.x, -m_position.y, -m_position.z);
-	//m_Parent->getTransform()->setPosition(-camX, -camY, -camZ);
-	//camPos = vec3(-camX, -camY, -camZ);
+void Camera::strafeLeft(){
+	m_position -= movementSpeed * strafedirection;
+}
+
+void Camera::strafeRight()
+{
+	m_position += movementSpeed * strafedirection;
 }
 
 MovementType Camera::setMovementType(MovementType type){
 	return m_movementType = type;
 }
-
-
-
-
-
-//use later for frame rate
-void Camera::setTime(float deltatime)
-{
-	m_deltatime = deltatime;
-}
-
-void Camera::setMousePosition(int mouseX, int mouseY)
-{
-	//Center the mouses position
-	m_MouseX = mouseX;
-	m_MouseY = mouseY;
-}
-
-
 
 //End Methods
 
